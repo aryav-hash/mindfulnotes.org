af666370175a9ccf829bb5dc59d2126472e3f6a3
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-07-12 12:29:46 +0530

    Used react router to setup the home-page of the website.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import home from './pages/home'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
   return (
     <>
       <Router>
-        <Route path="/" element={<Home />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Router>
     </>
   )
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <>
+            <h1 className="text-4xl text-center"> Welcome to Mindfull Notes </h1>
+        </>
+    )
+}
+
+export default Home
