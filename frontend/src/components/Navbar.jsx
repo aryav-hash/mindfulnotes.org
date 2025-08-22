dfbe866d05a003a0f52a4f51b83ddfe96bcf1748
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-08-22 15:34:05 +0530

    fix: Changed navbar to sticky and altered the margin of the hero section.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <>
-            <header className="top-0 fixed bg-white py-2 w-full shadow-sm shadow-green-100 hover:shadow-green-300">
+            <header className="top-0 sticky bg-white py-2 w-full shadow-sm shadow-green-100 hover:shadow-green-300">
                 <div className="max-w-6xl mx-auto px-4 flex justify-between items-center">
                 <h3 className="text-xl md:text-2xl font-bold text-green-600">Mindfulnotes</h3>
                 <nav className="space-x-6 hidden md:flex">
