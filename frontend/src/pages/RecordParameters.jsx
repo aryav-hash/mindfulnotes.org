c5c901f70131f32c41562c85d38ca83b4d8f5839
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-08-26 19:52:01 +0530

    feat: Completed all the sections of the form, now I need to fix some bugs and make the form function fully.

--- frontend/src/pages/RecordParameters.jsx
+++ frontend/src/pages/RecordParameters.jsx
@@ -4,12 +4,37 @@ import '../index.css';
 
 export default function RecordParameters() {
     const [sleep, setSleep] = useState(7);
+    const [choice, setChoice] = useState('');
+    const [items, setItems] = useState([]);
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+
+    const addItem = (e) => {
+        e.preventDefault();
+        if (!name || !price) return ;
+        name.replace(/\s+/g, " ").trim;
+        const checkNum = name.replace(" ", '');
+        if (!isNaN(parseInt(checkNum))) {
+            console.log(parseInt(checkNum));
+            alert("Please input a valid item name");
+            setName("");
+            setPrice("");
+            return ;
+        }
+        price.trim();
+        setItems([...items, {name, price}]);
+        setName("");
+        setPrice("");
+    };
+    
+    const del = (e, index) => {
+        e.preventDefault();
+        setItems(items.filter((_, i) => i !== index));
+    };
 
     return (
         <>
             <Navbar />
-            {/* Header section */}
-
             {/* Form section */}
             <section className="my-8 md:my-12 lg:my-15 flex flex-col items-center h-auto m-auto py-6 px-6 sm:py-10 max-w-xs smm:max-w-md 
             md:max-w-2xl lg:max-w-3xl border-3 border-green-400 rounded-3xl">
@@ -18,8 +43,8 @@ export default function RecordParameters() {
                 Parameters</h1>
 
                 <form className="space-y-2 smm:space-y-3 sm:space-y-4 w-full max-w-xl">
+                    
                     {/* Sleep input */}
-
                     <div className="grid grid-col-1">
                         <p className="text-sm smm:text-md md:text-xl font-bold">1. SLEEP</p>
                         <div className="mt-2 ml-5 w-full">
@@ -57,7 +82,7 @@ export default function RecordParameters() {
 
                     {/* Mood during the entire day */}
                     <div className="grid grid-col-1">
-                        <p className="pt-3 text-sm smm:text-md md:text-xl font-bold">2. MOOD</p>
+                        <p className="pt-8 text-sm smm:text-md md:text-xl font-bold">2. MOOD</p>
                         <div className="mt-2 ml-5 w-full">
                             <p htmlFor="sleep" className="text-xs smm:text-md md:text-lg text-wrap font-medium mb-1 
                             max-w-3xs smm:max-w-xs md:max-w-lg">~ How would you describe your mood throughout the day ?</p>
@@ -118,7 +143,7 @@ export default function RecordParameters() {
                                 <input type="checkbox" name="habits" className="mr-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600" />
                                 <label htmlFor="meditation" className="text-xs smm:text-md md:text-lg">Meditation / Relaxation</label>
                             </div>
-                            <div className="flex items-center mb-1">
+                            <div className="flex items-center">
                                 <input type="checkbox" name="habits" className="mr-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600" />
                                 <label htmlFor="learning" className="text-xs smm:text-md md:text-lg">Reading / Learning / Creative activity</label>
                             </div>
@@ -126,14 +151,54 @@ export default function RecordParameters() {
                     </div>
 
                     {/* financial data */}
-                        
+                    <div className="grid grid-col-1">
+                        <p className="pt-3 text-sm smm:text-md md:text-xl font-bold">5. FINANCES</p>
+                        <div className="mt-2 ml-5 w-full">
+                            <p htmlFor="finance-question" className="text-xs smm:text-md md:text-lg text-wrap font-medium mb-1 
+                            max-w-3xs smm:max-w-xs md:max-w-lg">~ Did you make any impulsive purchases ?</p>
+                            <div className="flex items-center mb-1">
+                                <input type="radio" value="yes" name="response" checked={choice === 'yes'} 
+                                onChange={(e) => {setChoice(e.target.value)}}
+                                className="mr-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600" />
+                                <label htmlFor="yes" className="text-xs smm:text-md md:text-lg">Yes</label>
+                            </div>
+                            <div className="flex items-center">
+                                <input type="radio" value="no" name="response" defaultChecked checked={choice === 'no'} 
+                                onChange={(e) => {setChoice(e.target.value)}}
+                                className="mr-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600" />
+                                <label htmlFor="no" className="text-xs smm:text-md md:text-lg">No</label>
+                            </div>
+                        </div>
 
+                        {/* conditional section */}
+                        {choice === "yes" && (
+                            <div className="mt-2 ml-5">
+                                <p htmlFor="finance-question" className="text-xs smm:text-md md:text-lg text-wrap font-medium mb-1 
+                            max-w-3xs smm:max-w-xs md:max-w-lg">Please provide some details:-</p>
+                                <div className="flex flex-col gap-2">
+                                    <input type="text" placeholder="Item name" maxLength="35" value={name} onChange={(e) => setName(e.target.value)}
+                                    className="text-xs smm:text-md md:text-lg border px-2 py-1 w-full max-w-[200px] smm:max-w-[300px] md:max-w-[400px] focus:outline-none focus:border-2 focus:border-green-500"/>
+                                    <input type="number" placeholder="Price" min="0" max="999999" value={price} onChange={(e) => setPrice(e.target.value)} 
+                                    className="text-xs smm:text-md md:text-lg border px-2 py-1 w-full max-w-[100px] smm:max-w-[150px] md:max-w-[180px] focus:outline-none focus:border-2 focus:border-green-500"/>
+                                    <button type="button" onClick={addItem} className="text-xs smm:text-md md:text-lg bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full max-w-[50px] smm:max-w-[65px] md:max-w-[80px]">
+                                        Add
+                                    </button>
+                                </div>
 
-                    <p>f</p>
-                    <p>g</p>
-                    <p>h</p>
+                                {/* display items */}
+                                <div className="mt-6 grid grid-cols-2 smm:grid-cols-3 w-full max-w-3xs smm:max-w-sm md:max-w-xl gap-4">
+                                    {items.map((item, index) => (
+                                        <div key={index} className="border rounded p-3 shadow-sm flex flex-col">
+                                            <span className="font-semibold text-xs smm:text-md md:text-lg">{item.name}</span>
+                                            <span className="text-xs smm:text-md md:text-lg text-gray-600">Rs {item.price}</span><br />
+                                            <button type="button" onClick={(e) => del(e, index)} className="text-xs smm:text-md md:text-lg text-red-500 hover:text-red-700">Delete</button>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
+                    </div>
                 </form>
-
             </section>
         </>
     );
