3cd94d794f1265cf26623af3cdf61b8d34f0c415
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-08-29 21:52:06 +0530

    fix: Trying to fix the deployment issue.

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/quote')
+@app.route('/api/quote')
 def get_quote():
     response = requests.get('https://zenquotes.io/api/random')
     return jsonify(response.json())
