535ba8a1721e7e539a02fc0c67d4e182adb0c673
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-08-29 19:25:41 +0530

    fix: All the routes are setup correctly and the server is also configured.

--- frontend/src/components/QuoteSection.jsx
+++ frontend/src/components/QuoteSection.jsx
@@ -5,7 +5,7 @@ export default function QuoteSection() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const api = "http://localhost:5000/api/quote";
+        const api = "/api/quote";
         async function getApi(url) {
             try {
                 const res = await fetch(url);
