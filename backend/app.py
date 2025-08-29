90ad2f23871cba02bf9861ebd88e1a3049efe313
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-08-29 20:20:10 +0530

    fix: Trying to fix the backend failure so that it can show the quote.

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/quote')
+@app.route('/quote')
 def get_quote():
     response = requests.get('https://zenquotes.io/api/random')
     return jsonify(response.json())
