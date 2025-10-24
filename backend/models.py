2e6255188de89b50eb2704f2b6e760d3d2e5c1a0
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-10-24 16:12:56 +0530

    feat: Removed the cache of the vite, updated .gitignore to ignore .venv and __pycache__ files and other files.

--- backend/models.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-class UserRecord(db.Model):
-    __tablename__ = 'records'
-    record_id = db.Column(db.Integer, primary_key=True)
-    
--- frontend/src/components/MenuBtn.jsx
+++
@@ -1,9 +0,0 @@
-export default function MenuBtn() {
-    return (
-        <>
-            <button className="bg-blue-600 text-white px-2 py-1 rounded-md hover:bg-blue-700 md:hidden">
-                Menu
-            </button>
-        </>
-    );
-};
